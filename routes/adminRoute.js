cd5ef138fda0845e9efc0acc45362911d6d12630
ChamNol09 <maochamnol37@gmail.com>
2026-06-25 10:57:21 +0700

    fix at seed admin

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,11 +3,35 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 const { isLogin } = require("../middlewares/auth");
 const { authoriz } = require("../middlewares/role");
-const { apiLimiter } = require('../middlewares/rateLimit')
+const { apiLimiter } = require("../middlewares/rateLimit");
 
-router.get("/users", apiLimiter, isLogin, authoriz("admin"), adminController.getAllUsers);
-router.delete('/user/:id', apiLimiter, isLogin, authoriz("admin"),adminController.deleteUser);
-router.get('/tasks', apiLimiter, isLogin, authoriz("admin"), adminController.getAllTasks);
-router.delete('/task/:id', apiLimiter, isLogin, authoriz("admin"), adminController.deleteTask);
+router.get(
+  "/users",
+  apiLimiter,
+  isLogin,
+  authoriz("Admin"),
+  adminController.getAllUsers,
+);
+router.delete(
+  "/user/:id",
+  apiLimiter,
+  isLogin,
+  authoriz("Admin"),
+  adminController.deleteUser,
+);
+router.get(
+  "/tasks",
+  apiLimiter,
+  isLogin,
+  authoriz("Admin"),
+  adminController.getAllTasks,
+);
+router.delete(
+  "/task/:id",
+  apiLimiter,
+  isLogin,
+  authoriz("Admin"),
+  adminController.deleteTask,
+);
 
 module.exports = router;
