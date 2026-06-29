02ac2899e6a09bcf591ac0fffc43a43d23b854fe
ChamNol09 <maochamnol37@gmail.com>
2026-06-29 16:01:30 +0700

    add upload and remove avatar

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const dotenv = require('dotenv');
-const express = require('express');
+const dotenv = require("dotenv");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const authRoute = require('./routes/auth');
-const adminRoute = require('./routes/adminRoute');
-const taskRoute = require('./routes/taskRoute');
-const deadlineJob = require('./jobs/deadlineJob')
-const telegramRoute = require('./routes/telegramRouter')
-const helmet = require('helmet');
-const userRoute = require('./routes/user.route');
+const cors = require("cors");
+const authRoute = require("./routes/auth");
+const adminRoute = require("./routes/adminRoute");
+const taskRoute = require("./routes/taskRoute");
+const deadlineJob = require("./jobs/deadlineJob");
+const telegramRoute = require("./routes/telegramRouter");
+const helmet = require("helmet");
+const userRoute = require("./routes/user.route");
+const path = require("path");
 dotenv.config();
 require("./telegrams/bot");
 
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
-  })
+  }),
 );
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/task', taskRoute);
-app.use('/api/telegram', telegramRoute);
-app.use('/api/avatar', userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/task", taskRoute);
+app.use("/api/telegram", telegramRoute);
+app.use("/api/avatar", userRoute);
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
