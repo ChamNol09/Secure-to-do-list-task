c788047eb5bfdfa0339e8542ff793730b521b4fa
ChamNol09 <maochamnol37@gmail.com>
2026-06-30 14:28:14 +0700

    fix on rate limit

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -13,7 +13,7 @@ const apiLimiter = rateLimit({
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 10,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
